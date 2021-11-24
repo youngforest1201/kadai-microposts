75428e5441b9831573922b750c817d8703ae114d
keiichi.wakamori <nrj02091@nifty.com>
2021-11-24 09:19:31 +0000

    post

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -24,10 +24,30 @@ class UsersController extends Controller
         // idの値でユーザを検索して取得
         $user = User::findOrFail($id);
 
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+
+        // ユーザの投稿一覧を作成日時の降順で取得
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+
         // ユーザ詳細ビューでそれを表示
         return view('users.show', [
             'user' => $user,
+            'microposts' => $microposts,
         ]);
     }
+    public function destroy($id)
+    {
+        // idの値で投稿を検索して取得
+        $micropost = \App\Micropost::findOrFail($id);
 
+        // 認証済みユーザ（閲覧者）がその投稿の所有者である場合は、投稿を削除
+        if (\Auth::id() === $micropost->user_id) {
+            $micropost->delete();
+        }
+
+        // 前のURLへリダイレクトさせる
+        return back();
+    }
+    
 }
